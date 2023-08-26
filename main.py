472a31be5e78c3ac7edc12b480f06c0914fce360
kirill <shkolenko02-03-2004@mail.ru>
2023-08-26 15:25:53 +0300

    Add base64 format

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import base64
 from pathlib import Path
 
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from starlette.middleware.cors import CORSMiddleware
 from shemas import GeneratePresentation
 
@@ -16,7 +17,15 @@ app.add_middleware(
 )
 
 
-@app.post("/api/v1/generate")
-async def generate_presentation(request: GeneratePresentation):
+@app.post("/api/v1/binary/generate")
+async def generate_presentation_binary(request: GeneratePresentation):
     pdf = Path("test.pdf")
     return FileResponse(pdf, headers={"Content-Disposition": "attachment"})
+
+
+@app.post("/api/v1/base64/generate")
+async def generate_presentation_base64(request: GeneratePresentation):
+    pdf = Path("test.pdf")
+    with open(pdf, 'rb') as pdf_file:
+        base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+    return JSONResponse(content={"pdf": base64_pdf})
