cb38b8bcc848168cd0fe8f846285701a7b705a9e
kirill <shkolenko02-03-2004@mail.ru>
2023-08-26 17:15:53 +0300

    Update request item in generate

--- shemas/presentation.py
+++ shemas/presentation.py
@@ -1,6 +1,24 @@
+from typing import List
 from pydantic import BaseModel
 
 
+class TeamMember(BaseModel):
+    name: str
+    role: str
+
+
+class Economy(BaseModel):
+    revenue: float
+    number_of_clients: float
+    APRU: int
+    churn_rate: int
+    LT: int
+    LTV: int
+
+
 class GeneratePresentation(BaseModel):
-    product: str | None = None
-    country: str | None = None
+    product_idea: str
+    team: List[TeamMember]
+    market: str
+    roadmap: str
+    economy: Economy
